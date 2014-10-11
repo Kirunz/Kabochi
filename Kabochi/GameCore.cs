08f6c3e5dace7c04c18931906a6592346945a674
Kirunz <kirunz@gmail.com>
2014-10-11 13:38:05 +1100

    Statring
    Создан проект с базовым классом GameCore

---
+++ Kabochi/GameCore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kabochi
+{
+    class GameCore
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Under construcion");
+            Console.ReadLine();
+        }
+    }
+}
